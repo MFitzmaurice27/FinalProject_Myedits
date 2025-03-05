5cb38b61ab147be38d5def7c5af1705e6ec1d554
MFitzmaurice27 <fitzmaum@union.edu>
2025-03-05 15:40:22 -0500

    Update temp_sensor.cpp

--- FinalProject/modules/temp_sensor/temp_sensor.cpp
+++ FinalProject/modules/temp_sensor/temp_sensor.cpp
@@ -1,172 +1,89 @@
-/*
- * mlx90614.c
- *
- *  Created on: Sep 13, 2023
- *      Author: fex95
- */
-
+/* This driver was written by Reinhard Krisam in 2023. The license can be found in the header file "MLX90614.h" */
 
 #include "temp_sensor.h"
 
-/**
-  * @brief Initialize the MLX90614 sensor.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  * @retval HAL status (indicates whether the sensor is ready to communicate).
-  */
-uint8_t MLX90614_init(MLX90614* sensor_obj){
-	// Turn on power to the sensor to start it
-	HAL_GPIO_WritePin(sensor_obj->power_gpio, sensor_obj->power_gpio_pin, GPIO_PIN_SET);
-	HAL_Delay(200);
 
-	// Calculate the internal I2C address
-	sensor_obj->address_internal = sensor_obj->address << 1;
+/* register addresses */
+const uint8_t regTempAmbient = ram_TA;
+const uint8_t regTempObject_1 = ram_Tobj_1;
 
-	// Check if the sensor is ready to communicate
-	return HAL_I2C_IsDeviceReady(sensor_obj->interface, sensor_obj->address_internal, 3, 5);
+/* buffer for reading the register */
+uint8_t regBuffer[3] = { 0x00, 0x00, 0x00 };
+
+/* variables for storing the register value and calculating the temperature */
+uint16_t tempAmbj, tempObj;
+float floatTemp_Amb, floatTemp_Object;
+
+
+
+/* this function reads the temperature registers from the MLX90614 and calculates the temperature in celsius */
+uint8_t MLX90614_Test_readTemp(I2C_HandleTypeDef *I2C_Module, uint16_t device_address, float* t_Ambient, float* t_Object)
+{
+	/* read the t_abmient */
+	MLX90614_readRegister(I2C_Module, device_address, regTempAmbient, (uint8_t *) &regBuffer[0]);
+
+	/* store the buffered register in an uint16_t variable (because the register value is actually 16 bit) */
+	tempAmbj = 0x00;
+	tempAmbj |= ((uint16_t) regBuffer[0]);
+	tempAmbj |= ((uint16_t) regBuffer[1]) << 8;
+
+	/* store the register value (uint16_t) in an float variable for the next conversation step */
+	floatTemp_Amb = (float) tempAmbj;
+
+	/* calculate the temperature in celsius */
+	floatTemp_Amb *= 0.02;
+	floatTemp_Amb -= 273.15;
+
+	/* pass the calculated temperature value to the pointer */
+	*t_Ambient = floatTemp_Amb;
+
+	/* read the t_object */
+	MLX90614_readRegister(I2C_Module, device_address, regTempObject_1, (uint8_t *) &regBuffer[0]);
+
+	/* store the buffered register in an uint16_t variable (because the register value is actually 16 bit) */
+	tempObj = 0x00;
+	tempObj |= ((uint16_t) regBuffer[0]);
+	tempObj |= ((uint16_t) regBuffer[1]) << 8;
+
+	/* store the register value (uint16_t) in an float variable for the next conversation step */
+	floatTemp_Object = (float) tempObj;
+
+	/* calculate the temperature in celsius */
+	floatTemp_Object *= 0.02;
+	floatTemp_Object -= 273.15;
+
+	/* pass the calculated temperature value to the pointer */
+	*t_Object = floatTemp_Object;
+
+	/* right now just return 0 -> error handling: tbd... */
+    return 0;
 }
 
-/**
-  * @brief Restart the MLX90614 sensor.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  */
-void MLX90614_restart(MLX90614* sensor_obj){
-	// Turn off power to the sensor
-	HAL_GPIO_WritePin(sensor_obj->power_gpio, sensor_obj->power_gpio_pin, GPIO_PIN_RESET);
-	HAL_Delay(500);
+/* this function reads the registers from the MLX90614 */
+uint8_t MLX90614_readRegister(I2C_HandleTypeDef *I2C_Module, uint16_t device_address, uint16_t reg_address, uint8_t *regValue)
+{
 
-	// Turn on power to the sensor to restart it
-	HAL_GPIO_WritePin(sensor_obj->power_gpio, sensor_obj->power_gpio_pin, GPIO_PIN_SET);
-	HAL_Delay(200);
-}
+	/* start the I2C transmission (the STM32 is the MASTER that WRITES data) to send data to the MLX90614 without a STOP at the end of the transmission */
+	HAL_I2C_Master_Seq_Transmit_IT(I2C_Module, device_address, (uint8_t *) &reg_address, (uint16_t) 0x0001, I2C_FIRST_FRAME);
 
-/**
-  * @brief Calculate CRC-8 checksum for data.
-  * @param addr Pointer to the data buffer.
-  * @param len Length of the data buffer.
-  * @retval CRC-8 checksum value.
-  */
-uint8_t MLX90614_crc8(uint8_t *addr, uint8_t len){
-	uint8_t crc = 0;
-	while (len--) {
-		uint8_t inbyte = *addr++;
-		for (uint8_t i = 8; i; i--) {
-			uint8_t carry = (crc ^ inbyte) & 0x80;
-			crc <<= 1;
-			if (carry)
-				crc ^= 0x7;
-			inbyte <<= 1;
-		}
-  }
-  return crc;
-}
+	/* wait for the I2C module to be ready again (the I2C related interrupts will be handled by the I2C-HAL internal state-machine) */
+    while (HAL_I2C_GetState(I2C_Module) != HAL_I2C_STATE_READY)
+    {
+    }
 
-/**
-  * @brief Write a 16-bit value to the MLX90614's with PEC.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  * @param reg Register address.
-  * @param value 16-bit value to write.
-  */
-void MLX90614_writeWord_with_PEC(MLX90614* sensor_obj, uint8_t reg, uint16_t value){
-	// Create a message with I2C address, register, value and add the PEC
-	uint8_t msg[5];
-	msg[0] = sensor_obj->address_internal;
-	msg[1] = reg;
-	msg[2] = value & 0xff;
-	msg[3] = value >> 8;
-	msg[4] = MLX90614_crc8(msg, 4);
+    /* restart the I2C transmission (STM32 is the MASTER that now READS data) to read data from the MLX90614 with a STOP at the end of the transmission */
+	HAL_I2C_Master_Seq_Receive_IT(I2C_Module, device_address, (uint8_t *) &regValue[0], (uint16_t) 0x0003, I2C_LAST_FRAME);
 
-	// Write the message
-	HAL_I2C_Mem_Write(sensor_obj->interface, sensor_obj->address_internal, reg, 1, msg+2, 3, 10);
-}
-
-/**
-  * @brief Read a 16-bit value from the MLX90614's EEPROM.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  * @param reg Register address in the EEPROM.
-  * @retval 16-bit value read from the EEPROM.
-  */
-uint16_t MLX90614_readEEPROM(MLX90614* sensor_obj, uint8_t reg){
-	uint8_t result[2];
-	HAL_I2C_Mem_Read(sensor_obj->interface, sensor_obj->address_internal, reg|0b00100000, 1, result, 2, 10);
-	return (uint16_t)result[1]<<8|result[0];
-}
-
-/**
-  * @brief Write a 16-bit value to the MLX90614's EEPROM with PEC and verify.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  * @param reg Register address in the EEPROM.
-  * @param value 16-bit value to write and verify.
-  * @retval 0 if the write and verification succeed, -1 if they fail.
-  */
-uint8_t MLX90614_writeEEPROM(MLX90614* sensor_obj, uint8_t reg, uint16_t value){
-	// Write a 16-bit value to the sensor's EEPROM with PEC
-	MLX90614_writeWord_with_PEC(sensor_obj, reg|0b00100000, 0x0000);
-	HAL_Delay(10);
-	MLX90614_writeWord_with_PEC(sensor_obj, reg|0b00100000, value);
-	HAL_Delay(10);
-
-	// Check if the value was written correctly
-	if(MLX90614_readEEPROM(sensor_obj, reg) != value)
-		return -1;
-
-	// If the address was written, update the sensor's address
-	if(reg == MLX90614_EEPROM_I2C_ADDRESS){
-		sensor_obj->address = value & 0xff;
-		sensor_obj->address_internal = sensor_obj->address << 1;
-	}
-
-	// Restart the sensor
-	MLX90614_restart(sensor_obj);
+	/* wait for the I2C module to be ready again (the I2C related interrupts will be handled by the I2C-HAL internal state-machine) */
+    while (HAL_I2C_GetState(I2C_Module) != HAL_I2C_STATE_READY)
+    {
+    }
+    /* right now just return 0 -> error handling: tbd... */
 	return 0;
+	
 }
-
-/**
-  * @brief Read a 16-bit value from the MLX90614's RAM.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  * @param reg Register address in the RAM.
-  * @param data Pointer to a buffer to store the read data (LSB first).
-  */
-void MLX90614_readRAM(MLX90614* sensor_obj, uint8_t reg, uint8_t* data){
-	HAL_I2C_Mem_Read(sensor_obj->interface,
-			sensor_obj->address_internal,
-			reg,
-			1,
-			data,
-			2,
-			10);
-}
-
-/**
-  * @brief Convert a temperature value in T-unit to degrees Celsius.
-  * @param bytes Pointer to a buffer containing the T-unit temperature data (LSB first).
-  * @param temperature Pointer to a variable to store the temperature in degrees Celsius.
-  */
-void MLX90614_tunitToDegreeC(uint8_t* bytes, float* temperature){
-	*temperature = ((uint16_t)bytes[1]<<8|bytes[0])*0.02f-273.15f;
-}
-
-/**
-  * @brief Read the ambient temperature from the MLX90614 sensor.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  * @param data Pointer to a variable to store the temperature in degrees Celsius.
-  */
-void MLX90614_readAmbientTemperature(MLX90614* sensor_obj, float* data){
-	// Read the ambient temperature from the sensor and convert it to degrees Celsius
-	uint8_t bytes[2];
-	MLX90614_readRAM(sensor_obj, MLX90614_RAM_T_AMBIENT, bytes);
-	MLX90614_tunitToDegreeC(bytes, data);
-}
-
-/**
-  * @brief Read the object temperature from the MLX90614 sensor.
-  * @param sensor_obj Pointer to the MLX90614 object.
-  * @param data Pointer to a variable to store the temperature in degrees Celsius.
-  * @param object number MLX90614_OBJx
-  */
-void MLX90614_readObjTemperature(MLX90614* sensor_obj, float* data, uint8_t obj_nr){
-	uint8_t bytes[2];
-	MLX90614_readRAM(sensor_obj,
-			(obj_nr == MLX90614_OBJ1) ? MLX90614_RAM_T_OBJ_1 : MLX90614_RAM_T_OBJ_2,
-			bytes);
-	MLX90614_tunitToDegreeC(bytes, data);
+uint8_t MLX90614_writeRegister(I2C_HandleTypeDef I2C_Module, uint16_t device_address, uint16_t reg_address, uint16_t regValue)
+{
+	/* todo */
+	return 0;
 }
